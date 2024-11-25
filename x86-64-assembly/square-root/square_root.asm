803b0b27c8d61faf829d47c351181fb8492ceaba
Denis Daletski <daletskidenis@gmail.com>
2024-11-25 19:46:46 +0100

    root: binsearch

--- x86-64-assembly/square-root/square_root.asm
+++ x86-64-assembly/square-root/square_root.asm
@@ -4,20 +4,39 @@ square_root:
     ; x in edi
     ; result in eax
 
+    ; ecx is low = 1
     xor ecx, ecx
-
-.loop:
     inc ecx
 
-    mov eax, ecx
-    mul ecx
+    ; esi is high = x
+    mov esi, edi
 
+.loop:
+    ; eax is mid = (low + high) / 2
+    mov eax, ecx
+    add eax, esi
+    shr eax, 1
+
+    mov r8d, eax
+
+    mul eax
     cmp eax, edi
-    jl .loop
+    jl .right
+    jg .left
 
-    mov eax, ecx
-
+    mov eax, r8d
     ret
+
+.left:
+    mov esi, r8d
+    dec esi
+    jmp .loop
+
+.right:
+    mov ecx, r8d
+    inc ecx
+    jmp .loop
+
 
 %ifidn __OUTPUT_FORMAT__,elf64
 section .note.GNU-stack noalloc noexec nowrite progbits
