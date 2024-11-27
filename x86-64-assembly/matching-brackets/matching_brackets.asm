84c11f33673ed79598e5a7e498872caec4d668a2
Denis Daletski <daletskidenis@gmail.com>
2024-11-27 21:33:59 +0100

    matching brackets update

--- x86-64-assembly/matching-brackets/matching_brackets.asm
+++ x86-64-assembly/matching-brackets/matching_brackets.asm
@@ -1,37 +1,24 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; A bit slower version using dynamic lookup table
-;;
-;; .text is 40% larger this way, but the 256-byte lookup table is now in .bss instead of .data,
-;; saving way more in return.
-;;
-;; The downside is that lookup table (well, only 6 of its entries) is now initialized at runtime on every `is_paired` call,
-;; which is probably negligible (it's just `O(1)`) for long enough test cases
-;;
-;; This solution also assumes .bss section is initialized with zeros at load time, which is required by ELF spec AFAIK
-;;
-;; Also, in multithreaded context, multiple threads share the lookup table and can write to it concurrently. But it shouldn't break
-;; anything as they'll write the same bytes anyway
-
 section .text
 global is_paired
 
 is_paired:
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ;; lookup initialization
-    lea rcx, [rel lookup]
-    mov byte [rcx + left_paren], 1
-    mov byte [rcx + left_sq], 1
-    mov byte [rcx + left_brace], 1
-    mov byte [rcx + right_paren], left_paren
-    mov byte [rcx + right_sq], left_sq
-    mov byte [rcx + right_brace], left_brace
+    lea rsi, [rel lookup]
+    mov byte [rsi + '('], 1
+    mov byte [rsi + '['], 1
+    mov byte [rsi + '{'], 1
+    mov byte [rsi + ')'], '('
+    mov byte [rsi + ']'], '['
+    mov byte [rsi + '}'], '{'
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     xor rdx, rdx
     xor rax, rax
     inc al ; brackets match until proven otherwise
 
-    mov rsi, rsp ; bracket stack bottom
+    push rbp
+    mov rbp, rsp ; bracket stack bottom
 
 .loop:
     mov dl, [rdi]
@@ -40,8 +27,7 @@ is_paired:
     test dl, dl
     jz .unwind ; string end reached
 
-    lea rcx, [rel lookup] ; lookup table address
-    mov cl, byte[rcx + rdx] ; table entry for a given char
+    mov cl, byte[rsi + rdx] ; table entry for a given char
 
     test cl, cl
     jz .loop ; some irrelevant symbol found, skip
@@ -53,7 +39,7 @@ is_paired:
     jmp .loop
 
 .closing:
-    cmp rsp, rsi
+    cmp rsp, rbp
     je .mismatch ; stack is empty, can't pop
 
     pop dx ; stack top is in `dl`
@@ -67,22 +53,16 @@ is_paired:
     xor al, al
 
 .unwind:
-    cmp rsp, rsi
+    cmp rsp, rbp
     je .end
-    mov rsp, rsi ; pop all values from stack
+    mov rsp, rbp ; pop all values from stack
     xor al, al ; brackets are mismatched if stack wasn't empty at the end
 
 .end:
+    pop rbp
     ret
 
 section .bss
-    left_paren: equ '('
-    right_paren: equ ')'
-    left_sq: equ '['
-    right_sq: equ ']'
-    left_brace: equ '{'
-    right_brace: equ '}'
-
 ;; lookup table for brackets
 ;; closing brackets lead to their opening counterparts
 ;; opening brackets lead to 1
